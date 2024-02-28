88432aa789cabf1969ce7df5f6bb82d2e837a23e
Daniel Subiabre <subiabrewd@gmail.com>
2024-02-28 13:36:27 +0100

    Make text look good

--- src/lib/ui/Content/Text.svelte
+++ src/lib/ui/Content/Text.svelte
@@ -1,11 +1,11 @@
-<div class="text-container">
+<div class="text-container bx--type-body-compact-02 ">
     <slot />
 </div>
 
 <style>
     div {
-        max-width: 66ch;
+        max-width: 69ch;
 
-        padding: 1rem;
+        padding: 1rem 1.375rem;
     }
 </style>

4e0264282a99767ef4bc7cc8736110f1a7718421
Daniel Subiabre <subiabrewd@gmail.com>
2024-03-10 20:20:43 +0100

    Allow Labeled component to have label at top or bottom

--- src/lib/ui/Content/Labeled.svelte
+++ src/lib/ui/Content/Labeled.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
     export let label: string;
+
+    export let bottom: boolean = false;
 </script>
 
 <p class="content-label-labeled">
-    <span class="content-label-label bx--form__helper-text">{label}</span>
-    <br />
-    <slot />
+    {#if bottom}
+        <slot />
+        <span class="content-label-label bx--form__helper-text">{label}</span>
+        <br />
+    {:else}
+        <span class="content-label-label bx--form__helper-text">{label}</span>
+        <br />
+        <slot />
+    {/if}
 </p>
 
 <style lang="scss">

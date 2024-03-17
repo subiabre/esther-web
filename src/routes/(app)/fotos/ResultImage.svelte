aea0f2d4ede112029261573795d894ec6f0c6bdb
Daniel Subiabre <subiabrewd@gmail.com>
2024-03-17 16:31:44 +0100

    Move image metadata modal out of overlaid

--- src/routes/(app)/fotos/ResultImage.svelte
+++ src/routes/(app)/fotos/ResultImage.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
     import type { CancelablePromise, Image, Photo } from "$lib/api";
     import Overlaid from "$lib/ui/Content/Overlaid.svelte";
+    import { CodeSnippet, Modal } from "carbon-components-svelte";
     import { api } from "../../../stores/api";
     import ResultImageActions from "./ResultImageActions.svelte";
     import ResultImageAltForm from "./ResultImageAltForm.svelte";
     import ResultImageDateRangeForm from "./ResultImageDateRangeForm.svelte";
+    import Labeled from "$lib/ui/Content/Labeled.svelte";
+    import FileSize from "$lib/ui/Content/FileSize.svelte";
 
     export let photo: Photo;
     export let source: string;
@@ -13,13 +16,35 @@
         method: "GET",
         url: source,
     });
+
+    let open: boolean = false;
 </script>
 
 <figure>
     {#await image then image}
         <img src={image.src} alt={image.alt} />
+        <Modal passiveModal modalHeading="Metadatos de la imagen" bind:open>
+            <Labeled label="Archivo">
+                {image.src}
+            </Labeled>
+            <Labeled label="Dimensiones">
+                {image.metadata?.width} x {image.metadata?.height}
+            </Labeled>
+            <Labeled label="Tamaño">
+                <FileSize size={image.metadata?.filesize || 0} />
+            </Labeled>
+            <Labeled label="MIME">
+                {image.metadata?.mimeType}
+            </Labeled>
+            <Labeled label="EXIF" />
+            <CodeSnippet
+                expanded
+                type="multi"
+                code={JSON.stringify(image.metadata?.exif, null, 2)}
+            />
+        </Modal>
         <Overlaid id={image.id || ""}>
-            <ResultImageActions {photo} {image} />
+            <ResultImageActions {photo} {image} on:metadata={() => open = !open} />
             <ResultImageDateRangeForm {image} />
             <ResultImageAltForm {image} />
         </Overlaid>
--- src/routes/(app)/fotos/ResultImageActions.svelte
+++ src/routes/(app)/fotos/ResultImageActions.svelte
@@ -1,43 +1,28 @@
 <script lang="ts">
     import type { Image, Photo } from "$lib/api";
-    import FileSize from "$lib/ui/Content/FileSize.svelte";
-    import Labeled from "$lib/ui/Content/Labeled.svelte";
     import Pad from "$lib/ui/Content/Pad.svelte";
-    import { Button, CodeSnippet, Modal } from "carbon-components-svelte";
+    import { Button } from "carbon-components-svelte";
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
 
     export let photo: Photo;
     export let image: Image;
 
     let imageIndex: number =
         photo.images.findIndex((value) => value.endsWith(`${image.id}`)) + 1;
-
-    let open: boolean = false;
 </script>
 
 <div>
     <span class="image-count">
         <Pad>{imageIndex} / {photo.images.length}</Pad>
     </span>
-    <Button title="Metadata" kind="ghost" on:click={() => (open = true)}>
+    <Button title="Metadata" kind="ghost" on:click={() => dispatch("metadata")}>
         🤖
     </Button>
     <a href={image.src} download="" target="_blank">
         <Button title="Descargar" kind="ghost">💾</Button>
     </a>
-    <Modal passiveModal modalHeading="Metadatos de la imagen" bind:open>
-        <Labeled label="Dimensiones">
-            {image.metadata?.width} x {image.metadata?.height}
-        </Labeled>
-        <Labeled label="Tamaño">
-            <FileSize size={image.metadata?.filesize || 0} />
-        </Labeled>
-        <Labeled label="EXIF" />
-        <CodeSnippet
-            expanded
-            type="multi"
-            code={JSON.stringify(image.metadata?.exif, null, 2)}
-        />
-    </Modal>
 </div>
 
 <style>

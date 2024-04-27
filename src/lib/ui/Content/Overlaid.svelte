bdb2a831a907cedec859e5fd8dc94a37120a561b
Daniel Subiabre <subiabrewd@gmail.com>
2024-04-28 00:39:52 +0200

    Update overlay to have configurable background and dynamic show state

--- src/lib/ui/Content/Overlaid.svelte
+++ src/lib/ui/Content/Overlaid.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     export let id: string | number;
+
+    export let show: boolean = false;
+
+    export let background: "gradient" | "solid" = "gradient";
 </script>
 
 <div class="overlaid">
     <label for="overlaid-status_{id}">
-        <input id="overlaid-status_{id}" type="checkbox" />
-        <div class="overlaid-content">
+        <input id="overlaid-status_{id}" type="checkbox" bind:checked={show} />
+        <div class="overlaid-content {background}">
             <slot />
         </div>
     </label>
@@ -43,6 +47,13 @@
         pointer-events: none;
         opacity: 0;
         transition: opacity 0.2s ease;
+    }
+
+    .overlaid-content.solid {
+        background-color: rgba(0, 0, 0, 0.85);
+    }
+
+    .overlaid-content.gradient {
         background: linear-gradient(
             90deg,
             rgba(0, 0, 0, 0.85) 0%,

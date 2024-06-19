7863d1127d565cccbe9aa042f51a86f1bfad527f
Daniel Subiabre <subiabrewd@gmail.com>
2024-06-19 23:47:36 +0200

    Allow to add new person from portrait

--- src/routes/(app)/fotos/ResultImagePortraitForm.svelte
+++ src/routes/(app)/fotos/ResultImagePortraitForm.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import type { Person, Portrait } from "$lib/api";
     import { api } from "$lib/stores/api";
+    import PeoplePortraitOption from "$lib/ui/Content/PeoplePortraitOption.svelte";
     import PeopleSearch from "$lib/ui/Content/PeopleSearch.svelte";
     import PeopleSelect from "$lib/ui/Content/PeopleSelect.svelte";
+    import { ClickableTile } from "carbon-components-svelte";
 
     export let portrait: Portrait;
     export let alignment: "top" | "bottom" = "top";
@@ -47,9 +49,39 @@
 
         person = new Promise((res) => res(undefined));
     }
+
+    let newPersonAdded: boolean = false;
+
+    async function newPerson() {
+        if (search === "") {
+            return;
+        }
+
+        person = $api.person.apiPeoplePost({
+            requestBody: {
+                name: search,
+                portraits: [`/v1/portraits/${portrait.id}`],
+            },
+        });
+
+        newPersonAdded = true;
+    }
 </script>
 
 {#if alignment === "top"}
+    {#if !newPersonAdded}
+        <ClickableTile
+            style="padding: 0 1rem 0 1rem; margin: 0;"
+            disabled={search === ""}
+            on:click={newPerson}
+        >
+            <PeoplePortraitOption
+                {portrait}
+                caption={search ? search : "Desconocido"}
+                label="Añadir nueva persona"
+            />
+        </ClickableTile>
+    {/if}
     {#await person then person}
         <PeopleSelect
             selected={person}
@@ -58,14 +90,17 @@
             on:deselect={removePerson}
         />
     {/await}
+    <PeopleSearch
+        bind:value={search}
+        on:update={(e) => (searchedPeople = e.detail.people)}
+    />
 {/if}
 
-<PeopleSearch
-    bind:value={search}
-    on:update={(e) => (searchedPeople = e.detail.people)}
-/>
-
 {#if alignment === "bottom"}
+    <PeopleSearch
+        bind:value={search}
+        on:update={(e) => (searchedPeople = e.detail.people)}
+    />
     {#await person then person}
         <PeopleSelect
             selected={person}
@@ -74,4 +109,17 @@
             on:deselect={removePerson}
         />
     {/await}
+    {#if !newPersonAdded}
+        <ClickableTile
+            style="padding: 0 1rem 0 1rem; margin: 0;"
+            disabled={search === ""}
+            on:click={newPerson}
+        >
+            <PeoplePortraitOption
+                {portrait}
+                caption={search ? search : "Desconocido"}
+                label="Añadir nueva persona"
+            />
+        </ClickableTile>
+    {/if}
 {/if}

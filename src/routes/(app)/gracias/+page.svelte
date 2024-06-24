3786c86a8dac046c65e2c9f684ed3b1b1032901d
Daniel Subiabre <subiabrewd@gmail.com>
2024-06-24 22:56:50 +0200

    Typos and format fixes for thank you page

--- src/routes/(app)/gracias/+page.svelte
+++ src/routes/(app)/gracias/+page.svelte
@@ -31,7 +31,7 @@
             </p>
             <p>
                 Una <a href="https://github.com/subiabre/esther-api">API</a>
-                REST con la invaluable ayuda de
+                web construida con
                 <a href="https://www.docker.com/">Docker</a>,
                 <a href="https://mariadb.org/">MariaDB</a>,
                 <a href="https://www.nginx.com/">Nginx</a>,
@@ -43,15 +43,11 @@
             </p>
             <p>
                 Gracias, también, a los mantenedores de
-                <a href="https://github.com/ScientaNL/DoctrineJsonFunctions">
-                    DoctrineJsonFunctions
-                </a>,
-                <a href="https://flysystem.thephpleague.com/docs/">
-                    Flysystem
-                </a>,
-                <a href="https://image.intervention.io/v3">
-                    Intervention Image
-                </a>
+                <a href="https://github.com/ScientaNL/DoctrineJsonFunctions"
+                    >DoctrineJsonFunctions</a
+                >,
+                <a href="http://flysystem.thephpleague.com/docs/">Flysystem</a>,
+                <a href="http://image.intervention.io/v3">Intervention Image</a>
                 y
                 <a href="https://github.com/loilo/Fuse">Fuse</a> por acelerar mi
                 trabajo.
@@ -61,51 +57,46 @@
                     >cliente web</a
                 >, la aplicación que estás viendo ahora, funciona gracias a
                 <a href="https://kit.svelte.dev/">Svelte Kit</a> con
-                <a href="https://sass-lang.com/">Sass</a> y
-                <a href="https://www.typescriptlang.org/">TypeScript</a>. Usa
-                además:
+                <a href="https://www.typescriptlang.org/">TypeScript</a> sobre
+                <a href="https://nodejs.org/en">Node.js</a>. Usa además:
             </p>
             <p>
                 La librería de componentes UI de IBM
                 <a href="https://svelte.carbondesignsystem.com/">Carbon</a>
-                para Svelte.
-            </p>
-            <p>
-                El buscador <a href="https://nominatim.org/">Nominatim</a> para
+                para Svelte y el preprocesador de CSS
+                <a href="https://sass-lang.com/">Sass</a>; el buscador
+                <a href="https://nominatim.org/">Nominatim</a>
+                para
                 <a href="https://www.openstreetmap.org/about">OpenStreetMap</a>
                 y mapas de
-                <a href="https://github.com/openlayers/openlayers">
-                    OpenLayers
-                </a>.
-            </p>
-            <p>
-                Y los paquetes
-                <a
-                    href="https://github.com/simeydotme/svelte-range-slider-pips"
-                >
+                <a href="http://github.com/openlayers/openlayers">OpenLayers</a>
+                con el arte raster de
+                <a href="https://stadiamaps.com/">Stadia Maps</a>; además de los
+                paquetes de <a href="https://www.npmjs.com/">npm</a>:
+                <a href="http://github.com/simeydotme/svelte-range-slider-pips">
                     Svelte Range Slider
                 </a>
                 y
-                <a href="https://github.com/faisalman/ua-parser-js">
-                    UAParser.js
-                </a>
+                <a href="http://github.com/faisalman/ua-parser-js"
+                    >UAParser.js</a
+                >.
             </p>
             <h3>Personas.</h3>
-            <p>Las siguientes personas han hecho posible Esther.</p>
+            <p>Las siguientes personas han ayudado a que Esther sea posible.</p>
             <p>
-                <a href="https://freesound.org/people/soundslikewillem/">
-                    Willem Sannen
-                </a>, por sus efectos de sonido.
+                <a href="https://freesound.org/people/soundslikewillem/"
+                    >Willem Sannen</a
+                >, por sus efectos de sonido.
             </p>
             <p>
-                <a href="https://www.instagram.com/stiftinhand">
-                    Su Shitong (Sonia)
-                </a>, por contribuir con su arte para el personaje de Esther.
+                <a href="https://www.instagram.com/stiftinhand"
+                    >Su Shitong (Sonia)</a
+                >, por contribuir con su arte para el personaje de Esther.
             </p>
             <p>
-                <a href="https://www.instagram.com/nazafalivene">
-                    Nazareno Falivene
-                </a>, por aportar ideas y criticismo durante la fase final de
+                <a href="https://www.instagram.com/nazafalivene"
+                    >Nazareno Falivene</a
+                >, por aportar ideas y criticismo durante la fase final de
                 desarrollo.
             </p>
             <p>
@@ -135,8 +126,8 @@
             </p>
             <p>
                 Denis (de nuevo), Evelyn (de nuevo) y Karina (de nuevo), Marcos,
-                Cristian, Gastón y Nora (de nuevo), por servir de
-                inspiración para este proyecto.
+                Cristian, Gastón y Nora (de nuevo), por servir de inspiración
+                para este proyecto.
             </p>
             <p>Mónica, sin la cual nada de esto hubiera ocurrido.</p>
         </Text>

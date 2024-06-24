aaf4b46b659a46061b7f54c06137b6710fed0f6b
Daniel Subiabre <subiabrewd@gmail.com>
2024-06-24 17:36:46 +0200

    Define api address via env var

--- src/lib/stores/api.ts
+++ src/lib/stores/api.ts
@@ -1,14 +1,15 @@
 import { ApiClient } from "$lib/api";
 import { writable } from "svelte/store";
 import { auth } from "./auth";
+import { API_ADDRESS } from "$env/static/private";
 
 export const api = writable(new ApiClient({
-    BASE: 'http://192.168.0.17:8080',
+    BASE: API_ADDRESS,
 }));
 
 auth.subscribe((auth) => {
     api.set(new ApiClient({
-        BASE: 'http://192.168.0.17:8080',
+        BASE: API_ADDRESS,
         TOKEN: auth.session?.token
     }))
 })

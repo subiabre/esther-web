e09bf57a22d7ca911a20ca1d20af26b2ab181934
Daniel Subiabre <subiabrewd@gmail.com>
2024-09-11 21:25:05 +0200

    Add big image component

---
+++ src/lib/ui/Content/BigImage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let src: string;
+
+    export let alt: string;
+</script>
+
+<img {src} {alt} />
+
+<style>
+    img {
+        max-width: 100%;
+        max-height: 100%;
+
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+</style>
